3d1e9b91243afe97d932946e26b5c748893686fb
Reva Artem <revaartem2008@gmail.com>
2023-02-07 15:15:13 +0200

    Add page to process a contact applications.

--- manager/urls.py
+++ manager/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import reservation_list, update_reservation
+from .views import reservation_list, update_reservation, contact_list, update_contact
 
 app_name = 'manager'
 
 urlpatterns = [
     path('reservations/', reservation_list, name='reservations_list'),
-    path('reservations/update/<int:pk>', update_reservation, name='update_reserve')
+    path('reservations/update/<int:pk>', update_reservation, name='update_reserve'),
+    path('contact/', contact_list, name='contact_list'),
+    path('contact/update/<int:pk>', update_contact, name='update_contact')
 ]
--- manager/views.py
+++ manager/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
-from main_page.models import UserReservation
+from main_page.models import UserReservation, ContactUs
 # Create your views here.
+
 
 def reservation_list(request):
     lst = UserReservation.objects.filter(is_processed=False)
@@ -8,6 +9,18 @@ def reservation_list(request):
         'lst': lst
     })
 
+
 def update_reservation(request, pk):
     UserReservation.objects.filter(pk=pk).update(is_processed=True)
     return redirect('manager:reservations_list')
+
+
+def contact_list(request):
+    all_contacts = ContactUs.objects.filter(is_processed=False)
+    return render(request, 'contact_list.html', context={
+        'all_contacts': all_contacts
+    })
+
+def update_contact(request, pk):
+    ContactUs.objects.filter(pk=pk).update(is_processed=True)
+    return redirect('manager:contact_list')
